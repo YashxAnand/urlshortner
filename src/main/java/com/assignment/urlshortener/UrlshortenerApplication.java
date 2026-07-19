0f5bfd3f4ee8d6b1fd0e4b72294b955e442b45eb
Yash <anandyash396@gmail.com>
2026-07-19 15:28:06 +0530

    Fix Spring Boot startup and component scan issues

--- src/main/java/com/assignment/urlshortener/UrlshortenerApplication.java
+++ src/main/java/com/assignment/urlshortener/UrlshortenerApplication.java
@@ -3,7 +3,10 @@ package com.assignment.urlshortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.ComponentScan;
+
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.assignment"})
 public class UrlshortenerApplication {
 
 	public static void main(String[] args) {
